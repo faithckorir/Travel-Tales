4152ed354f670d08335b67b7d6f2ecb77437e365
faithckorir <korir.faith@eclectics.io>
2023-08-22 11:46:56 +0300

    added firebase auth

--- app/src/main/java/com/faithckorir/mytraveltales/MainActivity.kt
+++ app/src/main/java/com/faithckorir/mytraveltales/MainActivity.kt
@@ -2,10 +2,35 @@ package com.faithckorir.mytraveltales
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.faithckorir.mytraveltales.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+        binding.signUpButton.setOnClickListener {
+            val email = "faithckorir@test.com"//emailEditText.text.toString()
+            val password = "faith123"//passwordEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Registration successful
+                        val user = auth.currentUser
+
+                        // You can perform additional actions here
+                    } else {
+                        // Registration failed
+                        Toast.makeText(baseContext, "Registration failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 }
